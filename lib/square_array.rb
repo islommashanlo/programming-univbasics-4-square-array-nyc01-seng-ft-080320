a6a148a150c5303345a5dad99bcc37d99fc4f2a8
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-27 15:44:59 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_element = array[counter] ** 2
+    new_element = array[counter].to_i ** 2
     new_array.push(new_element)
     counter += 1
   end
